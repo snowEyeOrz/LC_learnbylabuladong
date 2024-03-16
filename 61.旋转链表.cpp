b0f620b93d48b675cf7990af3a9d26fa3b9f4685
snowEyeOrz <prg4869@126.com>
2024-03-17 00:34:03 +0800

    commit change

--- 61.旋转链表.cpp
+++ 61.旋转链表.cpp
@@ -18,8 +18,53 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
+        if(head == nullptr) return head;
 
+        int n = getlength(head);
+        if(n == 1) return head;
+        if(k % n == 0) return head;
+        k = k % n;
+        ListNode* step1 = nullptr;
+        ListNode* step2 = nullptr;
+        ListNode* step3 = nullptr;
+
+        step1 = reverseN(head, n);
+        step2 = reverseN(step1, k);
+        step3 = reverseBetween(step2, k + 1, n);
+
+        return step3;
     }
+
+    int getlength(ListNode* head) {
+        int length = 0;
+        while (head != nullptr) {
+            length++;
+            head = head->next;
+        }
+        return length;
+    }
+
+    ListNode* successor = nullptr;
+    ListNode* reverseN(ListNode* head, int n) {
+        if (n == 1) {
+            successor = head->next;
+            return head;
+        }
+        ListNode* last = reverseN(head->next, n - 1);
+        head->next->next = head;
+        head->next = successor;
+        return last;
+    }
+
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        if (m == 1) {
+            return reverseN(head, n);
+        }
+
+        head->next = reverseBetween(head->next, m - 1, n - 1);
+        return head;
+    }
+
 };
 // @lc code=end
 
